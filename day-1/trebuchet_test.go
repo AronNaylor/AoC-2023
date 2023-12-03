2978f93e36986796ffe4f4e35a24894f3e502564
Aron Naylor <aron-naylor@outlook.com>
2023-12-03 15:34:36 +0000

    Add new tests for task 2

--- day-1/trebuchet_test.go
+++ day-1/trebuchet_test.go
@@ -6,6 +6,15 @@ package main
 // treb7uchet
 // In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
+//two1nine
+//eightwothree
+//abcone2threexyz
+//xtwone3four
+//4nineeightseven2
+//zoneight234
+//7pqrstsixteen
+//In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
 import (
 	"testing"
 )
@@ -22,6 +31,13 @@ func TestTrebuchet(t *testing.T) {
 		{"Line 2", "pqr3stu8vwx", 38},
 		{"Line 3", "a1b2c3d4e5f", 15},
 		{"Line 4", "treb7uchet", 77},
+		{"Line 5", "two1nine", 29},
+		//{"Line 6", "eightwothree", 83},
+		//{"Line 7", "abcone2threexyz", 13},
+		//{"Line 8", "xtwone3four", 24},
+		//{"Line 9", "4nineeightseven2", 42},
+		//{"Line 10", "zoneight234", 14},
+		//{"Line 11", "7pqrstsixteen", 76},
 	}
 
 	for _, tc := range lineTestCases {
